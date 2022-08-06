a86ab496f52cc2efd563400e6361ee9e505b169f
wzz <17bbayukari@gmail.com>
2022-08-07 01:52:22 +0800

    experiment: logistic_2

--- logistic/data_generation.py
+++ logistic/data_generation.py
@@ -1,18 +1,21 @@
 import numpy as np
 from abess import make_glm_data
 import pandas as pd
+import os
 
+np.random.seed(100401)
 df1 = []
 df2 = []
-n_list = range(500,2501,400)
+n_list = np.arange(100,600,20)
+p = 500
+k = 20
 
 for n in n_list:
-    for _ in range(1):
-        p = 10000
+    for _ in range(10):
         data = make_glm_data(
             n=n,
             p=p,
-            k=100,
+            k=k,
             rho=0.2,
             family="binomial",
             corr_type="exp",
@@ -34,5 +37,5 @@ for n in n_list:
         df.insert(0, 'id', len(df2))
         df2.append(df)
 
-pd.concat(df1, ignore_index=True).to_csv('info.csv')
-pd.concat(df2, ignore_index=True).to_csv('data.csv')
+pd.concat(df1, ignore_index=True).to_csv(os.path.dirname(__file__) + '/data/info.csv')
+pd.concat(df2, ignore_index=True).to_csv(os.path.dirname(__file__) + '/data/data.csv')
