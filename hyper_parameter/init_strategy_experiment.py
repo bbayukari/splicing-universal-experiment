bc6c174cc6b11c4fabaf27d4a74a8e4ed00bf81d
wzz <17bbayukari@gmail.com>
2022-12-06 21:32:40 +0800

    update hyper para EX

--- hyper_parameter/init_strategy_experiment.py
+++ hyper_parameter/init_strategy_experiment.py
@@ -1,87 +1,116 @@
 import numpy as np
 import time
+import statistic_model
+import statistic_model_pybind
+import parallel_experiment_util
 
-from abess import make_glm_data
-from abess import pybind_cabess
-from abess.universal import ConvexSparseSolver
-import sys
-sys.path.append("/data/home/wangzz/github/splicing-universal-experiment/")
-import MyTest
-from MyTest import merge_dict
-from MyTest import product_dict
+from abess import make_glm_data, ConvexSparseSolver
 
-pybind_cabess.init_spdlog(console_log_level=6, file_log_level=6)
+max_exchange_num_list = [2, 5, 10, 20, 30, 40, 50]
 
 
-def task(n ,p, k):
+def task(n, seed, model):
     result = {}
     # make dataset
-    data = make_glm_data(
-        n=n,
-        p=p,
-        k=k,
-        rho=0.2,
-        family="gaussian",
-        corr_type="exp",
-        snr=10 * np.log10(6),
-        standardize=True,
-    )
-    dataset = pybind_cabess.Data(data.x, data.y)
+    if model == "Ising":
+        p = 190
+        k = 40
+        data, theta, coef = statistic_model.ising_generator(P=20, N=n, Edges=k, seed=seed)
+        data_set = statistic_model_pybind.IsingData(data)
+    else:
+        p = 500
+        k = 50
+        coef = np.zeros(p)
+        np.random.seed(seed)
+        coef[np.random.choice(np.arange(p), k, replace=False)] = np.random.choice([100, -100], k)
+        data = make_glm_data(
+            n=n,
+            p=p,
+            k=k,
+            rho=0.2,
+            family="gaussian" if model == "Linear" else "binomial",
+            corr_type="exp",
+            snr=10 * np.log10(6),
+            standardize=True,
+            coef_=coef
+        )
+        data_set = statistic_model_pybind.RegressionData(data.x, data.y)
+
     # set model
-    model = ConvexSparseSolver(model_size=p, sample_size=n, intercept_size=1, support_size=k)
-    model.set_model_autodiff(
-        pybind_cabess.loss_linear,
-        pybind_cabess.gradient_linear,
-        pybind_cabess.hessian_linear,
+    solver = ConvexSparseSolver(
+        model_size=p, support_size=k
     )
-    model.set_data(dataset)
+
+    if model == "Ising":
+        solver.set_loss_custom(
+            loss = statistic_model_pybind.ising_loss,
+            gradient = statistic_model_pybind.ising_grad,
+            hessian = statistic_model_pybind.ising_hess_diag
+        )
+    elif model == "Classification":
+        solver.set_loss_custom(
+            statistic_model_pybind.logistic_loss_no_intercept,
+            statistic_model_pybind.logistic_gradient_no_intercept,
+            statistic_model_pybind.logistic_hessian_no_intercept,
+        )
+    elif model == "Linear":
+        solver.set_loss_custom(
+            statistic_model_pybind.linear_loss_no_intercept,
+            statistic_model_pybind.linear_gradient_no_intercept,
+            statistic_model_pybind.linear_hessian_no_intercept,
+        )
+    else:
+        raise ValueError("model must be one of Ising, Classification, Linear")    
+
     # run model
     t1 = time.time()
-    model.fit()
-    margin_coef = model.coef_
+    solver.fit(data_set)
+    margin_coef = solver.coef_
     t2 = time.time()
-    model.init_active_set = np.arange(p)
-    model.fit()
-    random_coef = model.coef_
+    solver.init_active_set = np.arange(p)
+    solver.fit(data_set)
+    random_coef = solver.coef_
     t3 = time.time()
-    model.init_active_set = [0]
-    model.fit()
-    sacrifice_coef = model.coef_
+    solver.init_active_set = [0]
+    solver.fit(data_set)
+    sacrifice_coef = solver.coef_
     t4 = time.time()
 
     # return
-    result["margin_accuracy"] = MyTest.accuracy(margin_coef, data.coef_)
+    result["margin_accuracy"] = parallel_experiment_util.accuracy(margin_coef, coef)
     result["margin_time"] = t2 - t1
-    result["random_accuracy"] = MyTest.accuracy(random_coef, data.coef_)
+    result["random_accuracy"] = parallel_experiment_util.accuracy(random_coef, coef)
     result["random_time"] = t3 - t2
-    result["sacrifice_accuracy"] = MyTest.accuracy(sacrifice_coef, data.coef_)
+    result["sacrifice_accuracy"] = parallel_experiment_util.accuracy(sacrifice_coef, coef)
     result["sacrifice_time"] = t4 - t3
     return result
 
 
 if __name__ == "__main__":
-    in_keys = ["n", "p", "k"]
+    in_keys = ["n", "seed", "model"]
     out_keys = [
         "margin_accuracy", "margin_time", "random_accuracy", "random_time", "sacrifice_accuracy", "sacrifice_time"
-    ] 
-    test = MyTest.Test(
-        task, in_keys, out_keys, processes=40, name="init_strategy"
-    )
-    # if n is very small, out of samples mse cann't be computed.
-    #test.check(n=200,p=1000,k=100)
+    ]
 
-    para = (
-        list(
-            MyTest.del_duplicate(
-                MyTest.product_dict(
-                    n=[i * 100 + 100 for i in range(10)], p=[500], k=[50]
-                ),
-                MyTest.product_dict(n=[500], p=[i * 100 + 100 for i in range(10)], k=[50])
-            )
-        )
-        * 10
+    experiment = parallel_experiment_util.ParallelExperiment(
+        task=task,
+        in_keys=in_keys,
+        out_keys=out_keys,
+        processes=40,
+        name="init_strategy",
+        memory_limit=80
     )
-    
-    #test start
-    test.run(para)
-    test.save()
+
+    if False:
+        experiment.check(n=1000, model="Linear", seed=1)
+        experiment.check(n=500, model="Classification", seed=10)
+        experiment.check(n=100, model="Ising", seed=100)
+    else:
+        parameters = parallel_experiment_util.para_generator(
+            {"n": [i * 100 + 100 for i in range(10)], "model": ["Linear", "Classification", "Ising"]},
+            repeat=100,
+            seed=1,
+        )
+        
+        experiment.run(parameters)
+        experiment.save()
